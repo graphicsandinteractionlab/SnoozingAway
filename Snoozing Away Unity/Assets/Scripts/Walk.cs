9eb1c5b1d02157035b46f219f5e4e9c15521a525
Hartmut Seichter <hartmut@technotecture.com>
2019-06-25 22:14:01 +0200

    MVP of the walker script - should now bring back the ray-cast method to support 'unusual' terrain

--- Snoozing Away Unity/Assets/Scripts/Walk.cs
+++ Snoozing Away Unity/Assets/Scripts/Walk.cs
@@ -9,6 +9,11 @@ public class Walk : MonoBehaviour
     private Cuboid cuboid;
 
     private int currentPos;
+    private int currentSide = 0;
+
+    private Vector3 [] sideDirections = {
+        Vector3.up,Vector3.left,Vector3.right,Vector3.down,Vector3.forward,Vector3.back
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +25,53 @@ public class Walk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            Reset();
+        } else if (Input.GetKeyUp(KeyCode.A)) {
+            currentSide = (currentSide + 1) % sideDirections.Length;
+            Debug.Log("Side:" + currentSide);
+        }
+
+        // update transforms
+        UpdateVisuals();
+
+    }
+
+    void UpdateVisuals()
+    {
+        // this is only an initial method - update to use original raycasting
+
+        // get side vector
+        Vector3 sideVector = sideDirections[currentSide];
+
+        // initial position from this point out we should cast the rays 
+        Vector3 worldPos = (Vector3)Cuboid.GetPosition(currentPos,cuboid.Dimensions) * cuboid.cellSize - cuboid.CenterPoint;
+
+        gameObject.transform.localPosition = worldPos + sideVector * cuboid.cellSize; // basically we occupy the other cell
+        gameObject.transform.localRotation = Quaternion.LookRotation(sideVector);
+
+    }
+
+    public void Reset()
+    {
+
+        Debug.Log("enabled: " + cuboid.GetEnabledCount);
+
+        for (var i = 0; i < cuboid.CellCount; i++) 
+        {
+            // the snoozer can be only attached to enabled cells 
+            // needs to be revised ...
+            if (cuboid.Cells[i].enabled == true) 
+            {
+                Debug.Log("found cell:" + i); 
+                
+                // need to check walkable or not 
+
+                currentPos = i;
+
+                return; // stop here
+            }
+        }
     }
 }
