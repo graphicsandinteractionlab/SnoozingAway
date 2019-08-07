2dd0b238172796e998ab836d9ca3e154a081c05f
Hartmut Seichter <hartmut@technotecture.com>
2019-08-07 12:02:40 +0200

    WiP

---
+++ Snoozing Away Unity/Assets/Scripts/Clinging.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clinging : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction =  target.transform.position - transform.position;
+        Debug.DrawRay(transform.position,direction,Color.yellow);
+        transform.rotation = Quaternion.LookRotation( direction );
+    }
+}
